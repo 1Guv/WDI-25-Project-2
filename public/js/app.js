0a9c6d49ef899503352c0366f2fff27053b2ee8a
Guv <Gurvinder.Singh.Sandhu@gmail.com>
2017-03-04 22:12:31 +0000

    got Register & find out more - buttons to work

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,6 @@ $(function () {
   var greeting = 'yo';
   console.log(greeting + ' Guv');
 
-  var tagLine1 = $('.tag-line-1').text;
+  var tagLine1 = $('#tag-line-1').text;
   console.log(tagLine1);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ $(() => {
   const greeting = 'yo';
   console.log(`${greeting} Guv`);
 
-  const tagLine1 = $('.tag-line-1').text;
+  const tagLine1 = $('#tag-line-1').text;
   console.log(tagLine1);
 
 });
