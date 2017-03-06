aed1eaaebf6df54356af7de3b9020c4b5e096d4e
Guv <Gurvinder.Singh.Sandhu@gmail.com>
2017-03-06 09:40:54 +0000

    used SetInterval to display different car models dynamically on the homepage

--- public/js/app.js
+++ public/js/app.js
@@ -8,13 +8,16 @@ $(function () {
   var $tagLine1 = $('#tag-line-1');
   console.log($tagLine1.text());
 
-  var listOfModels = ['Huracan', 'Gallardo', 'Enzo', 'La Ferrari', 'Novitec F12', '650S', '488 GTB', '456', 'F12 Berlinetta', 'DB11', 'i8', 'F40', '911 GT3', '918 Spyder', 'R8', 'GTR-Nismo', 'Agera R', 'Veyron', '675LT', 'AMG GTS', 'DB10', 'P1', 'One-77', 'CCR', 'Hennesy Venom GT', 'Pagani Huayra', 'Zenvo ST1', 'Koenigsegg CCX', 'Saleen S7 TT'];
+  var listOfModels = ['Huracan', 'Gallardo', 'Enzo', 'La Ferrari', 'Novitec F12', '650S', '488 GTB', '456', 'F12 Berlinetta', 'DB11', 'i8', 'F40', '911 GT3', '918 Spyder', 'R8', 'GTR-Nismo', 'Agera R', 'Veyron', '675LT', 'AMG GTS', 'DB10', 'P1', 'One-77', 'CCR', 'Hennesy Venom GT', 'Pagani Huayra', 'Zenvo ST1', 'Koenigsegg CCX', 'Saleen S7 TT', 'Velur'];
 
-  for (var i = 0; i < listOfModels.length; i++) {
-    setTimeout(function () {
-      $tagLine1.delay(4000).text(listOfModels[i]);
-    }, 4000);
-  }
+  var currentIndex = 0;
+
+  setInterval(function () {
+    console.log(listOfModels[currentIndex]);
+    $('#tag-line-1').text(listOfModels[currentIndex]);
+    currentIndex++;
+    if (currentIndex === listOfModels.length) currentIndex = 0;
+  }, 1000);
 
   // changeTagLine();
   //
--- src/js/app.js
+++ src/js/app.js
@@ -6,14 +6,16 @@ $(() => {
   const $tagLine1 = $('#tag-line-1');
   console.log($tagLine1.text());
 
-  const listOfModels = ['Huracan', 'Gallardo', 'Enzo', 'La Ferrari', 'Novitec F12', '650S', '488 GTB', '456', 'F12 Berlinetta', 'DB11', 'i8', 'F40', '911 GT3', '918 Spyder', 'R8', 'GTR-Nismo', 'Agera R', 'Veyron', '675LT', 'AMG GTS', 'DB10', 'P1', 'One-77', 'CCR', 'Hennesy Venom GT', 'Pagani Huayra', 'Zenvo ST1', 'Koenigsegg CCX', 'Saleen S7 TT'];
+  const listOfModels = ['Huracan', 'Gallardo', 'Enzo', 'La Ferrari', 'Novitec F12', '650S', '488 GTB', '456', 'F12 Berlinetta', 'DB11', 'i8', 'F40', '911 GT3', '918 Spyder', 'R8', 'GTR-Nismo', 'Agera R', 'Veyron', '675LT', 'AMG GTS', 'DB10', 'P1', 'One-77', 'CCR', 'Hennesy Venom GT', 'Pagani Huayra', 'Zenvo ST1', 'Koenigsegg CCX', 'Saleen S7 TT', 'Velur'];
 
+  let currentIndex = 0;
 
-  for (var i=0; i<listOfModels.length; i++) {
-    setTimeout(function() {
-      $tagLine1.delay(4000).text(listOfModels[i]);
-    }, 4000);
-  }
+  setInterval(() => {
+    console.log(listOfModels[currentIndex]);
+    $('#tag-line-1').text(listOfModels[currentIndex]);
+    currentIndex++;
+    if(currentIndex === listOfModels.length) currentIndex = 0;
+  }, 1000);
 
   // changeTagLine();
   //
